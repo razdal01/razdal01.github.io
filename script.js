8b5296ade365c9a6da7e04a3c7418e29d9317126
razdal01 <razdalofficial@gmail.com>
2026-01-26 11:52:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 const SUPABASE_URL = "https://thlhymoqiiohoosxngjg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRobGh5bW9xaWlvaG9vc3huZ2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MTI5NzUsImV4cCI6MjA4NDk4ODk3NX0.cqyxKvGKrfJrfbAh_yx0AygHynwWSVaZa1kg713ZdJg";
 
 const overlay = document.getElementById("overlay");
 const bgMusic = document.getElementById("bgMusic");
 const clickSound = document.getElementById("clickSound");
+const visitorText = document.getElementById("visitorCount");
 
 async function updateVisitors() {
   try {
@@ -29,19 +32,18 @@ async function updateVisitors() {
       body: JSON.stringify({ count })
     });
 
-    document.getElementById("visitorCount").innerText = count;
+    visitorText.innerText = count;
   } catch (e) {
     console.error(e);
-    document.getElementById("visitorCount").innerText = "error";
+    visitorText.innerText = "error";
   }
 }
 
-// CLICK TO VIEW
 overlay.addEventListener("click", () => {
   overlay.style.display = "none";
-
   if (clickSound) clickSound.play().catch(()=>{});
   if (bgMusic) bgMusic.play().catch(()=>{});
-
   updateVisitors();
+});
+
 });
